6efe98b6cbe65123c9b214621a8334c10087f147
santu <yeyeyedou@vip.qq.com>
2019-12-07 01:23:38 +0800

    测试家里提交

--- app.ts
+++ app.ts
@@ -23,6 +23,7 @@ app.use(function (err: any, req: any, res: any, next: any) {
 app.all("*", function (req, res, next) {
     next();
 });
+console.log('测试提交')
 openApi(app)
 app.listen(8889, () => {
     mongoose.connect('mongodb://localhost/mydb', (r) => {
